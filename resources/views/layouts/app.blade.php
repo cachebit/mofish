293f6cb8750b74d2eab439daa2bb2108f80ca709
cachebit <417209993@qq.com>
2017-08-03 13:20:52 +0000

    class 10 finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,18 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="/moyus">moyus</a></li>
+
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            Channel <span class="caret"></span>
+                          </a>
+                          <ul class="dropdown-menu">
+                            @foreach(App\Channel::all() as $channel)
+                              <li><a href="/moyus/{{ $channel->slug }}">{{ $channel->name }}</a></li>
+                            @endforeach
+                          </ul>
+                        </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
